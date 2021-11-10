35cf30e7d583785c191cd7692749363e74481d62
basemomar94 <basemomar2012@gmail.com>
2021-11-10 16:10:16 +0200

    new

--- app/src/main/java/com/example/gym_scanner/Activation.java
+++ app/src/main/java/com/example/gym_scanner/Activation.java
@@ -47,6 +47,11 @@ public class Activation extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -60,15 +65,7 @@ public class Activation extends AppCompatActivity {
             } else {
                 userID = result.getContents();
                 Toast.makeText(this, "Scanned: " + userID, Toast.LENGTH_LONG).show();
-                DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
-                Map<String, Object> add = new HashMap<>();
-                add.put("activation", "true");
-                documentReference.update(add).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-
-                    }
-                });
+                gettindata();
 
 
             }
@@ -77,6 +74,9 @@ public class Activation extends AppCompatActivity {
     }
 
     public void newact(View view) {
+
+
+
 
         IntentIntegrator intentIntegrator = new IntentIntegrator(this);
         intentIntegrator.setCaptureActivity(Capture.class);
@@ -90,6 +90,7 @@ public class Activation extends AppCompatActivity {
     }
 
     public void Edit(View view) {
+        updatinginfo();
         System.out.println("click");
         System.out.println(Editing);
         if (!Editing){
@@ -98,6 +99,7 @@ public class Activation extends AppCompatActivity {
             binding.infoLayout.setVisibility(View.INVISIBLE);
             binding.edit.setText("Save");
             Editing=true;
+
         } else {
             binding.editLayout.setVisibility(View.INVISIBLE);
             binding.infoLayout.setVisibility(View.VISIBLE);
@@ -105,5 +107,71 @@ public class Activation extends AppCompatActivity {
             Editing=false;
         }
 
+    }
+    void gettindata (){
+        binding.usercard.setVisibility(View.VISIBLE);
+        DocumentReference documentReference =firebaseFirestore.collection("users").document(userID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                String activation=value.getString("activation");
+                System.out.println(activation);
+                if (activation.equals("true")){
+                    binding.status.setText("This account is active");
+
+                } else {
+                    binding.status.setText("This account is inactive");
+                }
+                binding.userid.setText("User ID : "+userID);
+                binding.fnameRight.setText("first name : "+value.getString("fname"));
+                binding.fnameEdit.setText(value.getString("fname"));
+
+                binding.lnameRight.setText("last name : "+value.getString("lname"));
+                binding.lnameEdit.setText(value.getString("lname"));
+
+                binding.phoneRight.setText("phone : "+value.getString("phone"));
+                binding.phoneEdit.setText(value.getString("phone"));
+
+                binding.subscribtionDateRight.setText("Subscribtion date : "+value.getString("date"));
+                binding.subscribtionEdit.setText(value.getString("date"));
+
+             //   binding.daysRight.setText("number of days : "+value.getString("daysnumber"));
+
+
+
+
+            }
+        });
+    }
+
+    public void activateNow(View view) {
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+        Map<String, Object> add = new HashMap<>();
+        add.put("activation", "true");
+        documentReference.update(add).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+
+            }
+        });
+
+
+    }
+    void updatinginfo (){
+        DocumentReference documentReference =firebaseFirestore.collection("users").document(userID);
+        Map<String,Object> update = new HashMap<>();
+        update.put("fname",binding.fnameEdit.getText().toString().trim());
+        update.put("lname",binding.lnameEdit.getText().toString().trim());
+        update.put("phone",binding.phoneEdit.getText().toString().trim());
+        update.put("date",binding.subscribtionEdit.getText().toString().trim());
+        update.put("daysnumber",binding.daysEdit.getText().toString().trim());
+        documentReference.update(update).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(Activation.this,"Updates were saved",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
